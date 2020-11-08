0bc4ba4153427afe17b03f6055b8b8f382701dfa
msakuta <masahiro.sakuta@gmail.com>
2020-11-09 03:09:33 +0900

    Use url-loader to base64-embed small images into script

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,10 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
           },
         ],
       },
