ea85f493df63374b148984b8b4af93a09078496a
msakuta <masahiro.sakuta@gmail.com>
2020-11-11 00:52:19 +0900

    Split chest into source file

---
+++ src/chest.rs
@@ -0,0 +1,115 @@
+use super::structure::Structure;
+use super::{
+    DropItem, FactorishState, FrameProcResult, Inventory, InventoryTrait, ItemResponse,
+    ItemResponseResult, Position,
+};
+use wasm_bindgen::prelude::*;
+use web_sys::CanvasRenderingContext2d;
+
+const CHEST_CAPACITY: usize = 100;
+
+pub(crate) struct Chest {
+    position: Position,
+    inventory: Inventory,
+}
+
+impl Chest {
+    pub(crate) fn new(position: &Position) -> Self {
+        Chest {
+            position: *position,
+            inventory: Inventory::new(),
+        }
+    }
+}
+
+impl Structure for Chest {
+    fn name(&self) -> &'static str {
+        "Chest"
+    }
+
+    fn position(&self) -> &Position {
+        &self.position
+    }
+
+    fn draw(
+        &self,
+        state: &FactorishState,
+        context: &CanvasRenderingContext2d,
+        depth: i32,
+    ) -> Result<(), JsValue> {
+        if depth != 0 {
+            return Ok(());
+        };
+        let (x, y) = (self.position.x as f64 * 32., self.position.y as f64 * 32.);
+        match state.image_chest.as_ref() {
+            Some(img) => {
+                context.draw_image_with_image_bitmap(img, x, y)?;
+                Ok(())
+            }
+            None => Err(JsValue::from_str("chest image not available")),
+        }
+    }
+
+    fn desc(&self, _state: &FactorishState) -> String {
+        format!(
+            "Items: \n{}",
+            self.inventory
+                .iter()
+                .map(|item| format!("{:?}: {}<br>", item.0, item.1))
+                .fold(String::from(""), |accum, item| accum + &item)
+        )
+    }
+
+    fn item_response(&mut self, _item: &DropItem) -> Result<ItemResponseResult, ()> {
+        if self.inventory.len() < CHEST_CAPACITY {
+            self.inventory.add_item(&_item.type_);
+            Ok((
+                ItemResponse::Consume,
+                Some(FrameProcResult::InventoryChanged(self.position)),
+            ))
+        } else {
+            Err(())
+        }
+    }
+
+    fn input(&mut self, o: &DropItem) -> Result<(), JsValue> {
+        self.item_response(o)
+            .map(|_| ())
+            .map_err(|_| JsValue::from_str("ItemResponse failed"))
+    }
+
+    fn output<'a>(
+        &'a mut self,
+        state: &mut FactorishState,
+        position: &Position,
+    ) -> Result<(DropItem, Box<dyn FnOnce(&DropItem) + 'a>), ()> {
+        if let Some(ref mut item) = self.inventory.iter_mut().next() {
+            if 0 < *item.1 {
+                let item_type = item.0.clone();
+                Ok((
+                    DropItem {
+                        id: state.serial_no,
+                        type_: *item.0,
+                        x: position.x * 32,
+                        y: position.y * 32,
+                    },
+                    Box::new(move |_| {
+                        self.inventory.remove_item(&item_type);
+                    }),
+                ))
+            } else {
+                Err(())
+            }
+        } else {
+            Err(())
+        }
+    }
+
+    fn inventory(&self) -> Option<&Inventory> {
+        Some(&self.inventory)
+    }
+
+    fn inventory_mut(&mut self) -> Option<&mut Inventory> {
+        Some(&mut self.inventory)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #![allow(non_upper_case_globals)]
+mod chest;
 mod inserter;
 mod perlin_noise;
 mod structure;
 mod transport_belt;
 mod utils;
 
+use chest::Chest;
 use inserter::Inserter;
 use structure::Structure;
 use transport_belt::TransportBelt;
@@ -246,22 +248,6 @@ fn draw_direction_arrow(
     Ok(())
 }
 
-const CHEST_CAPACITY: usize = 100;
-
-struct Chest {
-    position: Position,
-    inventory: Inventory,
-}
-
-impl Chest {
-    fn new(position: &Position) -> Self {
-        Chest {
-            position: *position,
-            inventory: Inventory::new(),
-        }
-    }
-}
-
 fn item_to_str(type_: &ItemType) -> String {
     match type_ {
         ItemType::IronOre => "Iron Ore".to_string(),
@@ -293,98 +279,6 @@ fn str_to_item(name: &str) -> Option<ItemType> {
         "Furnace" => Some(ItemType::Furnace),
 
         _ => None,
-    }
-}
-
-impl Structure for Chest {
-    fn name(&self) -> &'static str {
-        "Chest"
-    }
-
-    fn position(&self) -> &Position {
-        &self.position
-    }
-
-    fn draw(
-        &self,
-        state: &FactorishState,
-        context: &CanvasRenderingContext2d,
-        depth: i32,
-    ) -> Result<(), JsValue> {
-        if depth != 0 {
-            return Ok(());
-        };
-        let (x, y) = (self.position.x as f64 * 32., self.position.y as f64 * 32.);
-        match state.image_chest.as_ref() {
-            Some(img) => {
-                context.draw_image_with_image_bitmap(img, x, y)?;
-                Ok(())
-            }
-            None => Err(JsValue::from_str("chest image not available")),
-        }
-    }
-
-    fn desc(&self, _state: &FactorishState) -> String {
-        format!(
-            "Items: \n{}",
-            self.inventory
-                .iter()
-                .map(|item| format!("{:?}: {}<br>", item.0, item.1))
-                .fold(String::from(""), |accum, item| accum + &item)
-        )
-    }
-
-    fn item_response(&mut self, _item: &DropItem) -> Result<ItemResponseResult, ()> {
-        if self.inventory.len() < CHEST_CAPACITY {
-            self.inventory.add_item(&_item.type_);
-            Ok((
-                ItemResponse::Consume,
-                Some(FrameProcResult::InventoryChanged(self.position)),
-            ))
-        } else {
-            Err(())
-        }
-    }
-
-    fn input(&mut self, o: &DropItem) -> Result<(), JsValue> {
-        self.item_response(o)
-            .map(|_| ())
-            .map_err(|_| JsValue::from_str("ItemResponse failed"))
-    }
-
-    fn output<'a>(
-        &'a mut self,
-        state: &mut FactorishState,
-        position: &Position,
-    ) -> Result<(DropItem, Box<dyn FnOnce(&DropItem) + 'a>), ()> {
-        if let Some(ref mut item) = self.inventory.iter_mut().next() {
-            if 0 < *item.1 {
-                let item_type = item.0.clone();
-                Ok((
-                    DropItem {
-                        id: state.serial_no,
-                        type_: *item.0,
-                        x: position.x * 32,
-                        y: position.y * 32,
-                    },
-                    Box::new(move |_| {
-                        self.inventory.remove_item(&item_type);
-                    }),
-                ))
-            } else {
-                Err(())
-            }
-        } else {
-            Err(())
-        }
-    }
-
-    fn inventory(&self) -> Option<&Inventory> {
-        Some(&self.inventory)
-    }
-
-    fn inventory_mut(&mut self) -> Option<&mut Inventory> {
-        Some(&mut self.inventory)
     }
 }
 
