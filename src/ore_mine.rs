d8a59e372e7ea921f9c08effbc606ce769fe5857
msakuta <masahiro.sakuta@gmail.com>
2020-11-19 21:54:16 +0900

    Don't crash when the mineral is exhausted under an ore mine

--- src/ore_mine.rs
+++ src/ore_mine.rs
@@ -2,7 +2,7 @@ use super::items::ItemType;
 use super::structure::{DynIterMut, Structure};
 use super::{
     draw_direction_arrow, DropItem, FactorishState, FrameProcResult, Position, Recipe, Rotation,
-    TempEnt,
+    TempEnt, COAL_POWER,
 };
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
@@ -11,7 +11,7 @@ use web_sys::CanvasRenderingContext2d;
 pub(crate) struct OreMine {
     position: Position,
     rotation: Rotation,
-    cooldown: f64,
+    progress: f64,
     power: f64,
     max_power: f64,
     recipe: Option<Recipe>,
@@ -22,9 +22,9 @@ impl OreMine {
         OreMine {
             position: Position { x, y },
             rotation,
-            cooldown: 0.,
-            power: 20.,
-            max_power: 20.,
+            progress: 0.,
+            power: 25., // TODO: Have some initial energy for debugging, should be zero
+            max_power: 25.,
             recipe: None,
         }
     }
@@ -64,17 +64,17 @@ impl Structure for OreMine {
         let tile = &state.board
             [self.position.x as usize + self.position.y as usize * state.width as usize];
         if let Some(_recipe) = &self.recipe {
-            let recipe_time = 80.;
             // Progress bar
             format!("{}{}{}{}{}",
-                format!("Progress: {:.0}%<br>", (recipe_time - self.cooldown) / recipe_time * 100.),
+                format!("Progress: {:.0}%<br>", self.progress * 100.),
                 "<div style='position: relative; width: 100px; height: 10px; background-color: #001f1f; margin: 2px; border: 1px solid #3f3f3f'>",
                 format!("<div style='position: absolute; width: {}px; height: 10px; background-color: #ff00ff'></div></div>",
-                    (recipe_time - self.cooldown) / recipe_time * 100.),
-                format!(r#"Power: <div style='position: relative; width: 100px; height: 10px; background-color: #001f1f; margin: 2px; border: 1px solid #3f3f3f'>
+                    self.progress * 100.),
+                format!(r#"Power: {:.1}kJ <div style='position: relative; width: 100px; height: 10px; background-color: #001f1f; margin: 2px; border: 1px solid #3f3f3f'>
                  <div style='position: absolute; width: {}px; height: 10px; background-color: #ff00ff'></div></div>"#,
-                  if 0. < self.max_power { (self.power) / self.max_power * 100. } else { 0. }),
-                format!("Expected output: {}", if 0 < tile.iron_ore { tile.iron_ore } else { tile.coal_ore }))
+                    self.power,
+                    if 0. < self.max_power { (self.power) / self.max_power * 100. } else { 0. }),
+                format!("Expected output: {}", if 0 < tile.iron_ore { tile.iron_ore } else if 0 < tile.coal_ore { tile.coal_ore } else { tile.copper_ore }))
         // getHTML(generateItemImage("time", true, this.recipe.time), true) + "<br>" +
         // "Outputs: <br>" +
         // getHTML(generateItemImage(this.recipe.output, true, 1), true) + "<br>";
@@ -99,21 +99,21 @@ impl Structure for OreMine {
                 self.recipe = Some(Recipe::new(
                     HashMap::new(),
                     hash_map!(ItemType::IronOre => 1usize),
-                    0.1,
+                    8.,
                     80.,
                 ));
             } else if 0 < tile.coal_ore {
                 self.recipe = Some(Recipe::new(
                     HashMap::new(),
                     hash_map!(ItemType::CoalOre => 1usize),
-                    0.1,
+                    8.,
                     80.,
                 ));
             } else if 0 < tile.copper_ore {
                 self.recipe = Some(Recipe::new(
                     HashMap::new(),
                     hash_map!(ItemType::CopperOre => 1usize),
-                    0.1,
+                    8.,
                     80.,
                 ));
             }
@@ -130,44 +130,63 @@ impl Structure for OreMine {
             //     }
             // }
 
-            let output =
-                |state: &mut FactorishState, item, position: &Position, cooldown: &mut f64| {
-                    if let Some(tile) = state.tile_at_mut(&[position.x, position.y]) {
-                        *cooldown = recipe.recipe_time;
-                        match item {
-                            ItemType::IronOre => tile.iron_ore -= 1,
-                            ItemType::CoalOre => tile.coal_ore -= 1,
-                            ItemType::CopperOre => tile.copper_ore -= 1,
-                            _ => (),
-                        }
+            let output = |state: &mut FactorishState, item, position: &Position| {
+                if let Ok(val) = if let Some(tile) = state.tile_at_mut(&[position.x, position.y]) {
+                    match item {
+                        ItemType::IronOre => Ok(&mut tile.iron_ore),
+                        ItemType::CoalOre => Ok(&mut tile.coal_ore),
+                        ItemType::CopperOre => Ok(&mut tile.copper_ore),
+                        _ => Err(()),
                     }
-                };
+                } else {
+                    Err(())
+                } {
+                    if 0 < *val {
+                        *val -= 1;
+                        Ok(*val)
+                    } else {
+                        Err(())
+                    }
+                } else {
+                    Err(())
+                }
+            };
 
             // Proceed only if we have sufficient energy in the buffer.
-            let progress = (self.power / recipe.power_cost).min(1.);
-            if state.rng.next() < progress / recipe.recipe_time * 5. {
+            let progress = (self.power / recipe.power_cost)
+                .min(1. / recipe.recipe_time)
+                .min(1. - self.progress);
+            if state.rng.next() < progress * 5. {
                 state
                     .temp_ents
                     .push(TempEnt::new(&mut state.rng, self.position));
             }
-            if self.cooldown < progress {
-                self.cooldown = 0.;
+            if 1. <= self.progress + progress {
+                self.progress = 0.;
                 let output_position = self.position.add(self.rotation.delta());
                 let mut str_iter = structures.dyn_iter_mut().map(|v| v);
                 if let Some(structure) = str_iter.find(|s| *s.position() == output_position) {
                     let mut it = recipe.output.iter();
                     if let Some(item) = it.next() {
-                        if structure
-                            .input(&DropItem {
-                                id: 1,
-                                type_: *item.0,
-                                x: output_position.x,
-                                y: output_position.y,
-                            })
-                            .is_ok()
-                        {
-                            output(state, *item.0, &self.position, &mut self.cooldown);
-                            return Ok(FrameProcResult::InventoryChanged(output_position));
+                        // Check whether we can input first
+                        if structure.can_input(item.0) {
+                            if let Ok(val) = output(state, *item.0, &self.position) {
+                                structure
+                                    .input(&DropItem {
+                                        id: 0,
+                                        type_: *item.0,
+                                        x: output_position.x,
+                                        y: output_position.y,
+                                    })
+                                    .or_else(|_| Err(()))?;
+                                if val == 0 {
+                                    self.recipe = None;
+                                }
+                                return Ok(FrameProcResult::InventoryChanged(output_position));
+                            } else {
+                                self.recipe = None;
+                                return Err(());
+                            };
                         }
                     }
                     if !structure.movable() {
@@ -178,20 +197,24 @@ impl Structure for OreMine {
                     // let dest_tile = state.board[dx as usize + dy as usize * state.width as usize];
                     let mut it = recipe.output.iter();
                     if let Some(item) = it.next() {
+                        assert!(it.next().is_none());
                         if let Err(_code) =
                             state.new_object(output_position.x, output_position.y, *item.0)
                         {
                             // console_log!("Failed to create object: {:?}", code);
                         } else {
-                            output(state, *item.0, &self.position, &mut self.cooldown);
+                            if let Ok(val) = output(state, *item.0, &self.position) {
+                                if val == 0 {
+                                    self.recipe = None;
+                                }
+                            }
                         }
-                        assert!(it.next().is_none());
                     } else {
                         return Err(());
                     }
                 }
             } else {
-                self.cooldown -= progress;
+                self.progress += progress;
                 self.power -= progress * recipe.power_cost;
             }
         }
@@ -210,8 +233,8 @@ impl Structure for OreMine {
 
     fn input(&mut self, item: &DropItem) -> Result<(), JsValue> {
         if item.type_ == ItemType::CoalOre && self.power == 0. {
-            self.max_power = 100.;
-            self.power = 100.;
+            self.max_power = COAL_POWER;
+            self.power = COAL_POWER;
             Ok(())
         } else {
             Err(JsValue::from_str("not inputtable to ore mine"))
