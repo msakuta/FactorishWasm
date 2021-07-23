2e98eb94a40533a088f08287410b862a2cdf6f8e
msakuta <masahiro.sakuta@gmail.com>
2021-07-24 08:18:36 +0900

    Put away macros into a dedicated source to clear the top of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,8 @@
 #![allow(non_upper_case_globals)]
 
-macro_rules! console_log {
-    ($fmt:expr, $($arg1:expr),*) => {
-        crate::log(&format!($fmt, $($arg1),+))
-    };
-    ($fmt:expr) => {
-        crate::log($fmt)
-    }
-}
-
-/// format-like macro that returns js_sys::String
-macro_rules! js_str {
-    ($fmt:expr, $($arg1:expr),*) => {
-        JsValue::from_str(&format!($fmt, $($arg1),+))
-    };
-    ($fmt:expr) => {
-        JsValue::from_str($fmt)
-    }
-}
-
-/// format-like macro that returns Err(js_sys::String)
-macro_rules! js_err {
-    ($fmt:expr, $($arg1:expr),*) => {
-        Err(JsValue::from_str(&format!($fmt, $($arg1),+)))
-    };
-    ($fmt:expr) => {
-        Err(JsValue::from_str($fmt))
-    }
-}
-
-macro_rules! hash_map {
-    { $($key:expr => $value:expr),+ } => {
-        {
-            let mut m = ::std::collections::HashMap::new();
-            $(
-                m.insert($key, $value);
-            )+
-            m
-        }
-    };
-}
+/// Macros needs to come first
+#[macro_use]
+mod macros;
 
 mod assembler;
 mod boiler;
